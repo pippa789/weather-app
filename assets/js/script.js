fb18f5c5402af4226a7a74abd7cbbebdae56f6e9
unknown <allenpip341@gmail.com>
2023-01-27 15:59:18 +0000

    rewrote fetc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
-var APIKey = 'd8e2a6ae07f5f62478cb2f71b9b2d15e'
-var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=' + APIKey;
 
-fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=' + APIKey)
+/// put this whole fetch squence in an event listenter on the search btn.
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=houston&limit=5&appid=d8e2a6ae07f5f62478cb2f71b9b2d15e')
 .then(response => response.json())
-.then(function(response){
-    console.log(response)
+.then(function(citiesResult){
+    let firstCity = citiesResult[0]
+    console.log(firstCity.lat)
+    console.log(firstCity.lon)
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=d8e2a6ae07f5f62478cb2f71b9b2d15e`)
 })
+
+    .then(response=>response.json())
+    .then(citiesReturned => {
+        console.log(citiesReturned)
+    })
