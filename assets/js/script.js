5d10f41de4efb62a6332b696139400a37d0e01c7
unknown <allenpip341@gmail.com>
2023-01-31 18:10:35 +0000

    just cant get the api call right, the geo finder is confusing me

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 
 /// put this whole fetch squence in an event listenter on the search btn.
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=houston&limit=5&appid=d8e2a6ae07f5f62478cb2f71b9b2d15e')
+let searchBtn = document.getElementById('search-button');
+searchBtn.addEventListener('click', search )
+function search(){
+
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=london&limit=5&appid=d8e2a6ae07f5f62478cb2f71b9b2d15e')
 .then(response => response.json())
 .then(function(citiesResult){
     let firstCity = citiesResult[0]
@@ -12,4 +16,12 @@ fetch('http://api.openweathermap.org/geo/1.0/direct?q=houston&limit=5&appid=d8e2
     .then(response=>response.json())
     .then(citiesReturned => {
         console.log(citiesReturned)
-    })
+        // for (let i = 0; i < citiesReturned.length; i++) {
+            
+        //     console.log(citiesReturned[i].city.list[0].weather)
+           let citiesArray = document.getElementById('search-form');
+            citiesArray.innerHTML = `<h1>${citiesReturned.city.list.weather}</h1>`
+            })
+//     })
+}
+    
